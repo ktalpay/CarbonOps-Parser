c284beee01252f67c3d16bbde646534b162343ad
Kürşat Alpay <kursat.alpay@gmail.com>
2026-05-08 11:41:17 +0300

    Create minimal .NET contracts baseline (#261)

---
+++ src/dotnet/CarbonOps.Parser.Contracts/ParserRunStatus.cs
@@ -0,0 +1,9 @@
+namespace CarbonOps.Parser.Contracts;
+
+public enum ParserRunStatus
+{
+    Pending = 0,
+    Running = 1,
+    Completed = 2,
+    Failed = 3,
+}
---
+++ src/dotnet/CarbonOps.Parser.Contracts/SourceDocumentStatus.cs
@@ -0,0 +1,9 @@
+namespace CarbonOps.Parser.Contracts;
+
+public enum SourceDocumentStatus
+{
+    Discovered = 0,
+    Downloaded = 1,
+    Failed = 2,
+    Skipped = 3,
+}
